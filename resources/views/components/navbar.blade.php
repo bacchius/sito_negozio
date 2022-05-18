3b0dc6d71051d0722b7148a456d92f5a9e2c6f27
Francesco Ruggiero <frn.rgg@gmail.com>
2022-05-18 18:15:48 +0200

    dipendenze, creazione componenti e navbar installate

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item pe-3">
+                <a class="nav-link" href="#">Azienda</a>
+                </li>
+                <li class="nav-item pe-3">
+                <a class="nav-link" href="#">Prodotti</a>
+                </li>
+                <li class="nav-item pe-3">
+                    <a class="nav-link" href="#">Consulenza Online</a>
+                </li>
+                <li class="nav-item pe-3">
+                    <a class="nav-link" href="#">Contatti</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
